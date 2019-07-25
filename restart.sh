05741599d9f78cd9b8b08e06056b1eea456cf164
ry-tanaka <tanaka@monogocoro.co.jp>
2019-07-25 17:24:31 +0900

    refine

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 VAR=`curl http://localhost:3000/ 2>&1 >/dev/null`
 if echo $VAR | grep -q 'Failed to connect'; then
   #kill -9 $(lsof -i tcp:3000 -t)
-  cd /var/www/AI-talk-engine/preprocessor; ~/bin/bundle exec rails server -d
+  cd /var/www/qa-preprocessor/; ~/bin/bundle exec rails server -d -e production
 fi
